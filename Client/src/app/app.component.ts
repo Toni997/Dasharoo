8367e4d605c67e6e42ca6d358897531d84fb5c08
Toni Kazinoti <tkazinoti@hrcloud.com>
2021-08-31 15:44:42 +0200

    music progress showing correctly, working on drag and drop

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,12 @@
 class AppController {
   title: String;
 
-  $onInit () {
-    this.title = 'app';
+  $onInit() {
+    this.title = "Dasharoo";
   }
-
 }
 
 export const AppComponent = {
   controller: AppController,
-  template: require('./app.component.html').default
+  template: require("./app.component.html").default,
 };
--- Client/src/app/music-player/music-player.component.ts
+++ Client/src/app/music-player/music-player.component.ts
@@ -1,31 +1,132 @@
 import "./music-player.component.scss";
 
 export class MusicPlayerController {
-  musicPlayer: HTMLAudioElement[];
-  timeline: any;
-  currentPercentage: number;
+  musicPlayerRef: any[];
+  mp: any;
+  timeline: HTMLDivElement[];
+  centerButtonIcon: "play" | "pause" = "play";
+  scope: any;
+  currentPercentage: number = 0;
+  volumeBar: any;
+  currentTime = 0;
+  duration = 0;
+  currentTimeString = "0:00";
+  durationString = "0:00";
+  scrubBar: HTMLDivElement[];
+  circle: HTMLDivElement[];
+  dragStartX: number;
+  dragStartCurrentProgress: number;
+
   constructor() {
-    "ngInject";
+    ("ngInject");
   }
 
   $onInit() {}
 
-  // $scope.$watch()
+  $postLink() {
+    this.musicPlayerRef[0].onloadedmetadata = () => {
+      this.mp = this.musicPlayerRef[0];
+    };
+  }
+
+  onDragStartCircle(e: any) {
+    e.dataTransfer.effectAllowed = "copyMove";
+    e.dataTransfer.dropEffect = "move";
+    this.dragStartX = e.x;
+    this.dragStartCurrentProgress =
+      (this.currentPercentage / 100) * this.timeline[0].offsetWidth;
+  }
+
+  onBeingDraggedCircle(e: any) {
+    // console.log(this.dragStartX, e.x);
+    let currentProgressWidth =
+      (this.currentPercentage / 100) * this.timeline[0].offsetWidth;
+    let howMuchToMove = e.x - this.dragStartX;
+    currentProgressWidth = this.dragStartCurrentProgress + howMuchToMove;
+    console.log(currentProgressWidth);
+
+    this.currentPercentage =
+      (currentProgressWidth / this.timeline[0].offsetWidth) * 100;
+  }
+
+  onDragEndCircle(e: any) {}
+
+  updateRecord() {
+    // this.mp.currentTime = this.scrubBar[0].value;
+  }
+
+  showCircle() {
+    this.circle[0].classList.remove("display-none");
+    this.circle[0].classList.add("display-block");
+  }
+
+  hideCircle() {
+    this.circle[0].classList.remove("display-block");
+    this.circle[0].classList.add("display-none");
+  }
+
+  doNothing(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  timeToString(time: number) {
+    let seconds = time;
+    let hours = 0;
+    let minutes = 0;
+    while (seconds >= 3600) {
+      seconds -= 3600;
+      hours++;
+    }
+    while (seconds >= 60) {
+      seconds -= 60;
+      minutes++;
+    }
+    if (hours === 0) {
+      return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    }
+    return (
+      hours +
+      ":" +
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  }
+
+  updateProgressBar() {
+    this.currentPercentage = (this.mp.currentTime / this.mp.duration) * 100;
+    let currentTime = Math.floor(this.mp.currentTime);
+    let duration = Math.floor(this.mp.duration);
+    if (this.currentTime !== currentTime) {
+      this.currentTimeString = this.timeToString(currentTime);
+      this.currentTime = currentTime;
+    }
+    if (this.duration !== duration) {
+      this.durationString = this.timeToString(duration);
+      this.duration = duration;
+    }
+  }
+
+  onEnded() {
+    this.centerButtonIcon = "play";
+  }
 
   changeTime(e: any) {
-    const mp: HTMLAudioElement = this.musicPlayer[0];
-
-    // let totalWidth = this.timeline[0].offsetWidth;
-    // let offsetWidth = e.offsetX;
-    // this.currentPercentage = (offsetWidth / totalWidth) * 100;
-
-    mp.currentTime = (this.currentPercentage / 100) * mp.duration;
+    let totalWidth = this.timeline[0].offsetWidth;
+    let offsetWidth = e.offsetX;
+    this.currentPercentage = (offsetWidth / totalWidth) * 100;
+    this.mp.currentTime = (this.currentPercentage / 100) * this.mp.duration;
   }
 
   playOrPause() {
-    const mp: HTMLAudioElement = this.musicPlayer[0];
-    if (mp.paused) mp.play();
-    else mp.pause();
+    if (this.mp.paused) {
+      this.mp.play();
+      this.centerButtonIcon = "pause";
+    } else {
+      this.mp.pause();
+      this.centerButtonIcon = "play";
+    }
   }
 }
 
